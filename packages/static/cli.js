b6bc8d0240b28ff1aa9525ec41b0946de601c680
Rune Madsen <rune@runemadsen.com>
2019-03-13 14:53:48 -0400

    this fixes #2

--- packages/static/cli.js
+++ packages/static/cli.js
@@ -16,14 +16,13 @@ const destroyFunc = require("./commands/destroy");
 
 const configFile = "./.staticconfig";
 
+const hasConfig = async () => {
+  return await access(configFile);
+};
+
 const loadConfig = async () => {
-  try {
-    await access(configFile);
-    const json = await readFile(configFile);
-    return JSON.parse(json);
-  } catch (e) {
-    return {};
-  }
+  const json = await readFile(configFile);
+  return JSON.parse(json);
 };
 
 const saveEnvironment = async (env, vars) => {
@@ -45,10 +44,31 @@ const init = async () => {
       default: "default"
     },
     {
-      type: "input",
+      type: "list",
       name: "awsRegion",
       message: "Name of AWS region for the S3 bucket",
-      default: "us-east-1"
+      default: "us-east-1",
+      choices: [
+        { name: "us-east-1 (Ohio)", value: "us-east-1" },
+        { name: "us-east-2 (N. Virginia)", value: "us-east-2" },
+        { name: "us-west-1 (N. California)", value: "us-west-1" },
+        { name: "us-west-2 (Oregon)", value: "us-west-2" },
+        { name: "ap-south-1 (Mumbai)", value: "ap-south-1" },
+        { name: "ap-northeast-1 (Tokyo)", value: "ap-northeast-1" },
+        { name: "ap-northeast-2 (Seoul)", value: "ap-northeast-2" },
+        { name: "ap-northeast-3 (Osaka)", value: "ap-northeast-3" },
+        { name: "ap-southeast-1 (Singapore)", value: "ap-southeast-1" },
+        { name: "ap-southeast-2 (Sydney)", value: "ap-southeast-2" },
+        { name: "ca-central-1 (Canada)", value: "ca-central-1" },
+        { name: "cn-north-1 (Beijing)", value: "cn-north-1" },
+        { name: "cn-northwest-1 (Ningxia)", value: "cn-northwest-1" },
+        { name: "eu-central-1 (Frankfurt)", value: "eu-central-1" },
+        { name: "eu-west-1 (Ireland)", value: "eu-west-1" },
+        { name: "eu-west-2 (London)", value: "eu-west-2" },
+        { name: "eu-west-3 (Paris)", value: "eu-west-3" },
+        { name: "eu-north-1 (Stockholm)", value: "eu-north-1" },
+        { name: "sa-east-1 (São Paulo)", value: "sa-east-1" }
+      ]
     },
     {
       type: "input",
@@ -74,9 +94,20 @@ const create = async args => {
     return console.error("Please state a name for this environment");
   }
 
+  if (!hasConfig()) {
+    console.error("Could not find .staticconfig file in current folder");
+    process.exit();
+  }
+
   const conf = await loadConfig();
   const env = args[3];
   const stackName = `${conf.websiteName}-${env}`;
+
+  // Check that this environment does not exist
+  if (conf.environments && conf.evironments.hasOwnProperty(env)) {
+    console.error("This environment already exists");
+    process.exit();
+  }
 
   // We don't prompt for these Cloudformation params,
   // but simply rely on their default values.
