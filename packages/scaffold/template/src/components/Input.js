462c407d178cd31d1cac434a275bee013cff4ee2
Rune Madsen <rune@runemadsen.com>
2019-03-20 13:39:35 +0100

    better components

--- packages/scaffold/template/src/components/Input.js
+++ packages/scaffold/template/src/components/Input.js
@@ -1,119 +1,111 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
-import { uid } from "../utils";
-import css from "./Input.css";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { uid } from '../utils';
+import css from './Input.css';
 
 export class Input extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      focus: false
-    };
-    this._id = uid("input");
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleOnFocus = this.handleOnFocus.bind(this);
-    this.handleOnBlur = this.handleOnBlur.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			focus: false
+		};
+		this._id = uid('input');
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleOnFocus = this.handleOnFocus.bind(this);
+		this.handleOnBlur = this.handleOnBlur.bind(this);
+	}
 
-  handleOnChange(event) {
-    const { name, value } = event.target;
-    const { onChange } = this.props;
-    onChange && onChange(event, name, value);
-  }
+	handleOnChange(event) {
+		const { name, value } = event.target;
+		const { onChange } = this.props;
+		onChange && onChange(event, name, value);
+	}
 
-  handleOnFocus() {
-    this.setState({ focus: true });
-  }
+	handleOnFocus() {
+		this.setState({ focus: true });
+	}
 
-  handleOnBlur() {
-    this.setState({
-      focus: false
-    });
-  }
+	handleOnBlur() {
+		this.setState({
+			focus: false
+		});
+	}
 
-  render() {
-    const {
-      disabled,
-      autocomplete,
-      error,
-      id = this._id,
-      invalid = false,
-      label,
-      name,
-      onKeyPress,
-      placeholder,
-      required = false,
-      type,
-      value
-    } = this.props;
+	render() {
+		const {
+			disabled,
+			autocomplete,
+			error,
+			id = this._id,
+			invalid = false,
+			label,
+			name,
+			onKeyPress,
+			placeholder,
+			required = false,
+			type,
+			value
+		} = this.props;
 
-    const { focus } = this.state;
-    const rootClasses = classnames(css.root, {
-      [css.invalid]: invalid,
-      [css.disabled]: disabled,
-      [css.focus]: focus
-    });
+		const { focus } = this.state;
+		const rootClasses = classnames(css.root, {
+			[css.invalid]: invalid,
+			[css.disabled]: disabled,
+			[css.focus]: focus
+		});
 
-    return (
-      <div style={css} className={rootClasses}>
-        {label && (
-          <label htmlFor={id} className={css.inputLabel}>
-            <span className={css.labelTxt}>{label}</span>
-            <input
-              autoComplete={autocomplete}
-              id={id}
-              name={name}
-              type={type}
-              value={value}
-              placeholder={placeholder}
-              disabled={disabled}
-              onFocus={this.handleOnFocus}
-              onBlur={this.handleOnBlur}
-              onChange={this.handleOnChange}
-              onKeyPress={onKeyPress}
-              aria-required={required}
-              aria-describedby={`error-${id}`}
-              aria-invalid={invalid}
-            />
-          </label>
-        )}
-        {invalid && error && (
-          <div className={css.errorWrapper}>
-            <span
-              id={`error-${id}`}
-              className={css.errorTxt}
-              aria-live="polite"
-            >
-              {error}
-            </span>
-          </div>
-        )}
-      </div>
-    );
-  }
+		return (
+			<div style={css} className={rootClasses}>
+				{label && <label htmlFor={id}>{label}</label>}
+				<input
+					autoComplete={autocomplete}
+					id={id}
+					name={name}
+					type={type}
+					value={value}
+					placeholder={placeholder}
+					disabled={disabled}
+					onFocus={this.handleOnFocus}
+					onBlur={this.handleOnBlur}
+					onChange={this.handleOnChange}
+					onKeyPress={onKeyPress}
+					aria-required={required}
+					aria-describedby={`error-${id}`}
+					aria-invalid={invalid}
+				/>
+				{invalid && error && (
+					<div className={css.errorWrapper}>
+						<span id={`error-${id}`} className={css.errorTxt} aria-live="polite">
+							{error}
+						</span>
+					</div>
+				)}
+			</div>
+		);
+	}
 }
 
 export default Input;
 
 Input.defaultProps = {
-  type: "text",
-  autocomplete: "off",
-  onChange: () => {}
+	type: 'text',
+	autocomplete: 'off',
+	onChange: () => {}
 };
 
 Input.propTypes = {
-  name: PropTypes.string,
-  value: PropTypes.string,
-  type: PropTypes.string,
-  label: PropTypes.string.isRequired,
-  autocomplete: PropTypes.string,
-  disabled: PropTypes.bool,
-  error: PropTypes.string,
-  id: PropTypes.string,
-  invalid: PropTypes.bool,
-  onChange: PropTypes.func,
-  onKeyPress: PropTypes.func,
-  placeholder: PropTypes.string,
-  required: PropTypes.bool
+	name: PropTypes.string,
+	value: PropTypes.string,
+	type: PropTypes.string,
+	label: PropTypes.string.isRequired,
+	autocomplete: PropTypes.string,
+	disabled: PropTypes.bool,
+	error: PropTypes.string,
+	id: PropTypes.string,
+	invalid: PropTypes.bool,
+	onChange: PropTypes.func,
+	onKeyPress: PropTypes.func,
+	placeholder: PropTypes.string,
+	required: PropTypes.bool
 };
--- packages/scaffold/template/src/components/Select.js
+++ packages/scaffold/template/src/components/Select.js
@@ -1,108 +1,106 @@
-import React, { Component, Fragment } from "react";
-import PropTypes from "prop-types";
-import classnames from "classnames";
-import { uid } from "../utils";
-import css from "./Select.css";
+import React, { Component, Fragment } from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
+import { uid } from '../utils';
+import css from './Select.css';
 
 export class Select extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { focus: false };
-    this._id = uid("select");
-    this.handleOnChange = this.handleOnChange.bind(this);
-    this.handleOnFocus = this.handleOnFocus.bind(this);
-    this.handleOnBlur = this.handleOnBlur.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = { focus: false };
+		this._id = uid('select');
+		this.handleOnChange = this.handleOnChange.bind(this);
+		this.handleOnFocus = this.handleOnFocus.bind(this);
+		this.handleOnBlur = this.handleOnBlur.bind(this);
+	}
 
-  handleOnChange(e) {
-    const { onChange } = this.props;
-    onChange && onChange(e, e.target.name, e.target.value);
-  }
+	handleOnChange(e) {
+		const { onChange } = this.props;
+		onChange && onChange(e, e.target.name, e.target.value);
+	}
 
-  handleOnFocus() {
-    this.setState({ focus: true });
-  }
+	handleOnFocus() {
+		this.setState({ focus: true });
+	}
 
-  handleOnBlur() {
-    this.setState({ focus: false });
-  }
+	handleOnBlur() {
+		this.setState({ focus: false });
+	}
 
-  render() {
-    const {
-      children,
-      defaultValue,
-      disabled,
-      error,
-      id = this._id,
-      invalid = false,
-      label,
-      name,
-      placeholder,
-      required = false,
-      value
-    } = this.props;
+	render() {
+		const {
+			children,
+			defaultValue,
+			disabled,
+			error,
+			id = this._id,
+			invalid = false,
+			label,
+			name,
+			placeholder,
+			required = false,
+			value
+		} = this.props;
 
-    const rootClasses = classnames(css.root, {
-      [css.focus]: this.state.focus,
-      [css.withLabel]: label,
-      [css.invalid]: invalid,
-      [css.disabled]: disabled
-    });
+		const rootClasses = classnames(css.root, {
+			[css.focus]: this.state.focus,
+			[css.withLabel]: label,
+			[css.invalid]: invalid,
+			[css.disabled]: disabled
+		});
 
-    return (
-      <Fragment>
-        <div className={rootClasses}>
-          {label && (
-            <label className={css.label} htmlFor={id}>
-              <span>{label}:</span>
-            </label>
-          )}
-          <select
-            id={id}
-            className={css.select}
-            disabled={disabled}
-            onChange={this.handleOnChange}
-            onFocus={this.handleOnFocus}
-            onBlur={this.handleOnBlur}
-            name={name}
-            defaultValue={defaultValue}
-            placeholder={placeholder}
-            value={value}
-            aria-required={required}
-            aria-describedby={`error-${id}`}
-            aria-invalid={invalid}
-          >
-            <option disabled>{placeholder}</option>
-            {children}
-          </select>
-        </div>
-        {invalid && error && (
-          <div className={css.error} id={`error-${id}`} aria-live="polite">
-            {error}
-          </div>
-        )}
-      </Fragment>
-    );
-  }
+		return (
+			<Fragment>
+				<div className={rootClasses}>
+					{label && (
+						<label htmlFor={id}>
+							<span>{label}:</span>
+						</label>
+					)}
+					<select
+						id={id}
+						disabled={disabled}
+						onChange={this.handleOnChange}
+						onFocus={this.handleOnFocus}
+						onBlur={this.handleOnBlur}
+						name={name}
+						defaultValue={defaultValue}
+						placeholder={placeholder}
+						value={value}
+						aria-required={required}
+						aria-describedby={`error-${id}`}
+						aria-invalid={invalid}>
+						<option disabled>{placeholder}</option>
+						{children}
+					</select>
+				</div>
+				{invalid && error && (
+					<div className={css.error} id={`error-${id}`} aria-live="polite">
+						{error}
+					</div>
+				)}
+			</Fragment>
+		);
+	}
 }
 
 export default Select;
 
 Select.defaultProps = {
-  placeholder: "Please choose..."
+	placeholder: 'Please choose...'
 };
 
 Select.propTypes = {
-  name: PropTypes.string,
-  value: PropTypes.string, // for controlled component
-  defaultValue: PropTypes.string, // for uncontrolled component
-  children: PropTypes.node,
-  disabled: PropTypes.bool,
-  error: PropTypes.string,
-  id: PropTypes.string,
-  invalid: PropTypes.bool,
-  label: PropTypes.string,
-  onChange: PropTypes.func,
-  placeholder: PropTypes.string,
-  required: PropTypes.bool
+	name: PropTypes.string,
+	value: PropTypes.string, // for controlled component
+	defaultValue: PropTypes.string, // for uncontrolled component
+	children: PropTypes.node,
+	disabled: PropTypes.bool,
+	error: PropTypes.string,
+	id: PropTypes.string,
+	invalid: PropTypes.bool,
+	label: PropTypes.string,
+	onChange: PropTypes.func,
+	placeholder: PropTypes.string,
+	required: PropTypes.bool
 };
