6a5363a914e374ab9ea949fc565581f25a5e57f5
Martin Bravo <bravomartin@gmail.com>
2020-08-27 18:12:12 -0400

    add pre-render with static-site-generator-webpack-plugin, helmet

--- packages/scaffold/template/src/pages/Home.js
+++ packages/scaffold/template/src/pages/Home.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 import { useApi } from '../hooks/api';
+
 import css from './Home.css';
 
 export default (props) => {
   const [data, loadData, status] = useApi('/get');
 
-  if (!status.loaded) {
-    return <p>Loading ...</p>;
-  }
-
-  // The api endpoint returns a JSON document with the headers
-  // Let's just show that in a list.
-  const list = Object.entries(data.headers).map((entry) => (
-    <li key={entry[0]}>
-      {entry[0]}: {entry[1]}
-    </li>
-  ));
-
   return (
     <div className={css.root}>
+      <Helmet>
+        <title>Homepage</title>
+      </Helmet>
       <Link to={'/page'}>Go to /page</Link>
       <h1>Sample API Response</h1>
-      <ul>{list}</ul>
+      <ul>
+        {status.loaded ? (
+          Object.entries(data.headers).map((entry) => (
+            <li key={entry[0]}>
+              {entry[0]}: {entry[1]}
+            </li>
+          ))
+        ) : (
+          <p>Loading ...</p>
+        )}
+      </ul>
     </div>
   );
 };
